f99c57ead860ef604479cc7d3ff5e0525fcb83d2
DavidVentura <davidventura27@gmail.com>
2023-06-08 12:22:20 +0200

    Do not render anything if the window has been terminated

--- test.c
+++ test.c
@@ -231,6 +231,11 @@ void HandleResume()
 {
 	suspended = 0;
 }
+
+void HandleWindowTermination() {
+       suspended = 1;
+ }
+
 void Callback( struct CNFADriver * sd, short * out, short * in, int framesp, int framesr )
 {
 	if(suspended) return;
